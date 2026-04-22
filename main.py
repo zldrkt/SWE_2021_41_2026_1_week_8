c2f2edd54d196eee8273f11490b21e2070e2eb23
zldrkt <ifnjtqifsf@gmail.com>
2026-04-22 18:24:27 +0900

    main initialized

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def even_list(int_list: List[int]) -> List[int]:
+    """Determines if a number is even and return an even list."""
+    # TODO: Implement even_list
+    pass
+
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    """Computes the sum of the squares of all even numbers in a list."""
+    # TODO: Implement sum_of_squares_of_even
+    pass
+
+def main():
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
+if __name__ == "__main__":
+    main()
